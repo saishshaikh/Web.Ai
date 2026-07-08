4efc2ec6fa4b7bb56bce49d8a99b407c53954f40
_Saish_23 <saishshaikh2323@gmail.com>
2026-07-08 19:55:17 +0530

    Update auth.contoller.js

--- Backend/Controllers/auth.contoller.js
+++ Backend/Controllers/auth.contoller.js
@@ -24,8 +24,8 @@ export const googleAuth = async (req, res) => {
     // cookies
     res.cookie ("token",token,{
       httpOnly :true,
-      secure : false,
-      sameSite : "strict",
+      secure : true,
+      sameSite : "none",
       maxAge : 7*24*60*60*1000
     })
 
